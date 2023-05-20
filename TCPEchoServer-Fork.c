6e5838e47e3bc5c40eca11837bf884bd45a34606
zemlianin <detulie@mail.ru>
2023-05-21 01:36:39 +0300

    2.0

--- TCPEchoServer-Fork.c
+++ TCPEchoServer-Fork.c
@@ -12,42 +12,23 @@
 #define RCVBUFSIZE 32
 
 sem_t *sem;
+sem_t *sems;
 float EPS = 0.01;
 float fun_param_1 = 1;
 float fun_param_2 = 2;
 float fun_param_3 = 3;
 
-double f(double x)
-{
-    return abs(fun_param_1 * x * x * x + fun_param_2 * x * x + fun_param_3 * x);
-}
-
-double q_integral(double left, double right, double f_left, double f_right, double intgrl_now)
-{
-    sleep(1); // чтобы успевать что-то видеть
-    double mid = (left + right) / 2;
-    double f_mid = f(mid); // Аппроксимация по левому отрезку
-
-    double l_integral = (f_left + f_mid) * (mid - left) / 2; // Аппроксимация по правому отрезку
-
-    double r_integral = (f_mid + f_right) * (right - mid) / 2;
-    if (abs((l_integral + r_integral) - intgrl_now) > EPS)
-    { // Рекурсия для интегрирования обоих значений
-        l_integral = q_integral(left, mid, f_left, f_mid, l_integral);
-        r_integral = q_integral(mid, right, f_mid, f_right, r_integral);
-    }
-
-    return (l_integral + r_integral);
-}
-
 int main(int argc, char *argv[])
 {
-    int servSock;                    /* Socket descriptor for server */
-    int clntSock;                    /* Socket descriptor for client */
+    int servSock;  /* Socket descriptor for server */
+    int clntSock;  /* Socket descriptor for client */
+    int servSock2; /* Socket descriptor for server */
+    int clntSock2;
     unsigned short echoServPort;     /* Server port */
     pid_t processID;                 /* Process ID from fork() */
     unsigned int childProcCount = 0; /* Number of child processes */
     const char *sem_name = "sem100";
+    const char *sem_s = "sem10";
     int SIZE = 256;
 
     int shm_fd;
@@ -77,6 +58,11 @@ int main(int argc, char *argv[])
         perror("sem_open: Can not create admin semaphore");
         exit(-1);
     };
+    if ((sems = sem_open(sem_s, O_CREAT, 0666, 1)) == 0)
+    {
+        perror("sem_open: Can not create admin semaphore");
+        exit(-1);
+    };
 
     if (argc != 2) /* Test for correct number of arguments */
     {
@@ -87,7 +73,7 @@ int main(int argc, char *argv[])
     echoServPort = atoi(argv[1]); /* First arg:  local port */
     servSock = CreateTCPServerSocket(echoServPort);
     int cur = 0;
-    bool flag = false;
+    int SIZE_F = sizeof(float);
 
     for (;;) /* Run forever */
     {
@@ -97,72 +83,74 @@ int main(int argc, char *argv[])
             DieWithError("fork() failed");
         else if (processID == 0) /* If this is the child process */
         {
-            close(servSock); /* Child closes parent socket */
-
+            close(servSock);             /* Child closes parent socket */
             char echoBuffer[RCVBUFSIZE]; /* Buffer for echo string */
             int recvMsgSize;             /* Size of received message */
-
-            while (cur <= 10)
+            char lb[SIZE_F];
+            char rb[SIZE_F];
+            char ansb[SIZE_F];
+            float l = 0;
+            float r = 0;
+            while (cur < 10)
             {
                 sem_wait(sem);
                 float cur = atof((char *)ptr);
                 sprintf(ptr, "%f", cur + 1);
                 sem_post(sem);
-                if (cur > 10)
+
+                if (cur >= 10)
                 {
+                    sprintf(lb, "%f", -1.0);
+                    sprintf(rb, "%f", -1.0);
+                    send(clntSock, lb, SIZE_F, 0);
+                    send(clntSock, rb, SIZE_F, 0);
                     break;
                 }
-                float l = left + cur * step;
-                float r = left + (cur + 1) * step;
-                // double area = q_integral(l, r, f(l), f(r), (f(l) + f(r)) * (r - l) / 2);
-                // printf("%f\t%f\t : %f\n", l, r, area);
-                int SIZE_F = 8;
-                char lb[SIZE_F];
-                char rb[SIZE_F];
+
+                l = left + cur * step;
+                r = left + (cur + 1) * step;
                 sprintf(lb, "%f", l);
                 sprintf(rb, "%f", r);
-                send(clntSock, lb, SIZE_F, 0);
-                send(clntSock, rb, SIZE_F, 0);
-                // recvMsgSize = recv(clntSock, echoBuffer, RCVBUFSIZE, 0);
-                // float ans = atof(echoBuffer);
-                // printf("%f\n", ans);
+                write(clntSock, lb, SIZE_F);
+                write(clntSock, rb, SIZE_F);
+
+                recvMsgSize = recv(clntSock, ansb, SIZE_F, 0);
+                float ans = atof(ansb);
+                sem_wait(sems);
+                printf("area: %f\n", ans);
+                float s = atof((char *)ptrs1);
+                sprintf(ptrs1, "%f", s + ans);
+                sem_post(sems);
             }
             if (sem_unlink(sem_name) == -1)
             {
                 perror("sem_unlink: Incorrect unlink of full semaphore");
                 exit(-1);
             };
-
-            /*if ((recvMsgSize = recv(clntSocket, echoBuffer, RCVBUFSIZE, 0)) < 0)
-                DieWithError("recv() failed");
-
-            while (recvMsgSize > 0)
-            {
-                if (send(clntSocket, echoBuffer, recvMsgSize, 0) != recvMsgSize)
-                    DieWithError("send() failed");
-                if ((recvMsgSize = recv(clntSocket, echoBuffer, RCVBUFSIZE, 0)) < 0)
-                    DieWithError("recv() failed");
-            }*/
-
-            close(clntSock); /* Close client socket */
-
-            exit(0); /* Child process terminates */
+            exit(0);
         }
 
-        printf("with child process: %d\n", (int)processID);
-        close(clntSock);  /* Parent closes child socket descriptor */
-        childProcCount++; /* Increment number of outstanding child processes */
+        close(clntSock);
+        childProcCount++;
 
-        while (childProcCount) /* Clean up all zombies */
+        while (childProcCount)
         {
-            processID = waitpid((pid_t)-1, NULL, WNOHANG); /* Non-blocking wait */
-            if (processID < 0)                             /* waitpid() error? */
+            processID = waitpid((pid_t)-1, NULL, WNOHANG);
+            if (processID < 0)
                 DieWithError("waitpid() failed");
-            else if (processID == 0) /* No zombie to wait on */
+            else if (processID == 0)
                 break;
             else
-                childProcCount--; /* Cleaned up after a child */
+                childProcCount--;
         }
+        float cur = atof((char *)ptr);
+        if (childProcCount == 0 && cur >= 10)
+        {
+            float s = atof((char *)ptrs1);
+            printf("all area: %f\n", s);
+            fflush(stdout);
+            exit(0);
+        }
+        // exit(0);
     }
-    /* NOT REACHED */
 }
